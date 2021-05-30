aa7689abadee703fac4bb5a54b6d402134e2ef41
MadSage1337 <emmanueldavies14@gmail.com>
2021-05-30 01:26:32 -0700

    1 commit

---
+++ Lessons.js
@@ -0,0 +1,92 @@
+let Lessons = [
+{
+    "id": 1001,
+    "image": './image/english.jpg',
+    "subject" : "English",
+    "location" :"London",
+    "price" : '$100',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1002,
+    "image": './image/math1.jpg',
+    "subject" : "Math",
+    "location" :"Dubai",
+    "price" : '$170',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1003,
+    "image": './image/art.jpg',
+    "subject" : "Art",
+    "location" :"Italy",
+    "price" : '$109',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1004,
+    "image": './image/geography.jpeg',
+    "subject" : "Geography",
+    "location" :"America",
+    "price" : '$100',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1005,
+    "image": './image/music.jpg',
+    "subject" : "Music",
+    "location" :"France",
+    "price" : '$170',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1006,
+    "image": './image/chemistry.jpg',
+    "subject" : "Chemistry",
+    "location" :"Oxford",
+    "price" : '$180',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1007,
+    "image": './image/english.jpg',
+    "subject" : "English",
+    "location" :"America",
+    "price" : '$100',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1008,
+    "image": './image/physics.jpg',
+    "subject" : "Physics",
+    "location" :"Oxford",
+    "price" : '$180',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1009,
+    "image": './image/art.jpg',
+    "subject" : "Art",
+    "location" :"France",
+    "price" : '$150',
+    "numberofspace" : 5,
+},
+
+{
+    "id": 1010,
+    "image": './image/arabic.jpeg',
+    "subject" : "Arabic",
+    "location" :"Dubai",
+    "price" : '$160',
+    "numberofspace" : 5,
+},
+
+]
